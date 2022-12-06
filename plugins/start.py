b18738c6cad650c56c4f1813dca64b6a28b9c8e1
ｒｙｍｅ <119275831+irymee@users.noreply.github.com>
2022-12-06 20:27:48 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,7 +6,7 @@ from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceRep
 import humanize
 from helper.database import  insert ,find_one
 from pyrogram.file_id import FileId
-CHANNEL = os.environ.get("CHANNEL", "")
+CHANNEL = os.environ.get("CHANNEL", "-1001741731458")
 import datetime
 
 #Part of Day --------------------
